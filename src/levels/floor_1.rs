a5ac62c0452871ca87239983ab973b8b660a56cb
Manoj Dasari <dasarimanoj265@gmail.com>
2024-08-28 17:10:47 +0530

    Added numbers

---
+++ src/levels/floor_1.rs
@@ -0,0 +1,7 @@
+pub fn numbers() {
+    let x:i8 = -12;
+    let y:u8 = 45;
+    let z:f32 = 245.98;
+
+    print!("x: {}, y: {}, z: {}", x, y, z);
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod levels {
+    pub mod floor_1;
+}
+
+use levels::floor_1::numbers;
+
+fn main() {
+    numbers();
+}
