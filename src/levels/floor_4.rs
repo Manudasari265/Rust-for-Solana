c03add11a2cae24469f66f4a7408a97c6375ab6a
Manoj Dasari <dasarimanoj265@gmail.com>
2024-08-29 23:29:21 +0530

    conditionals & loops

---
+++ src/levels/floor_4.rs
@@ -0,0 +1,16 @@
+// Conditionals and Loops
+
+pub fn ConditionalsAndLoops() {
+    let is_even = true;
+
+    if(is_even) {
+        println!("The number is even");
+    }
+    else{
+        println!("The number is odd");
+    }
+
+    for i in 0..11 {
+        print!("{} ", i);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ mod levels {
     pub mod floor_1;
     pub mod floor_2;
     pub mod floor_3;
+    pub mod floor_4;
 }
 
 use levels::floor_1::numbers;
 use levels::floor_2::booleans;
 use levels::floor_3::strings;
+use levels::floor_4::ConditionalsAndLoops;
 
 fn main() {
-    // numbers();
-    // booleans();
+    numbers();
+    booleans();
     strings();
+    ConditionalsAndLoops();
 }
