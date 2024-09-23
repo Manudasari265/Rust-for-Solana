2bd8d9a37231db7be8129b02afa85c4ab730dd4b
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-23 23:14:17 +0530

    another example for mutability

--- src/levels/floor_5.rs
+++ src/levels/floor_5.rs
@@ -4,4 +4,8 @@ pub fn mutability() {
     let mut x: i8 = 1;
     x = 3;
     println!("{}", x);
+
+    let mut y: String = String::from("Hi there");
+    y.push_str(" Rust");
+    println!("{}", y);
 }
