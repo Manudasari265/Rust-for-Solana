a289e21e785c3a1b5a51413ff49837f68a8782c7
Manoj Dasari <dasarimanoj265@gmail.com>
2024-10-14 23:00:25 +0530

    variables and mutability

--- src/levels/floor_5.rs
+++ src/levels/floor_5.rs
@@ -8,4 +8,25 @@ pub fn mutability() {
     let mut y: String = String::from("Hi there");
     y.push_str(" Rust");
     println!("{}", y);
+
+    //* 'const' keyword */
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; //^ always use uppercase with underscores between words
+
+    //* shadowing */
+    let i = 5;
+    let i = i + 1;
+    println!("The value above the scope is: {}", i);
+    {
+        let i = i * 2;
+        println!("The value of i in the inner scope is {}", i);
+    }
+    println!("The value of i in outer scope is: {}", i);
+
+    let spaces = "      ";
+    let spaces = spaces.len();
+    println!("The length of spaces is: {}", spaces);
+
+    let mut spaces1 = "      "; //^ this variable doesn't need to be mutable
+    // spaces1 = spaces1.len(); //^ this will give us error
+    println!("The spaces1 length is: {}", spaces1);
 }
