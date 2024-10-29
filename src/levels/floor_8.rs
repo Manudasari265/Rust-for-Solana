52684cc7ea2925d9f33fabcad1965220c8e9f70f
Manoj Dasari <dasarimanoj265@gmail.com>
2024-10-29 23:58:09 +0530

    error handling for code blocks impl

--- src/levels/floor_8.rs
+++ src/levels/floor_8.rs
@@ -1,15 +1,27 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+fn main() {
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+    
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("short");
+        result = longest(string1.as_str(), string2.as_str()); // usually error occurs here
+    }
+    println!("The longest string is: {}", result);
+
+    fn divide(a: f64, b: f64) -> Result<f64, String> {
+        if b == 0.0 {
+            Err(String::from("Division by zero"))
+        } else {
+            Ok(a / b)
+        }
     }
 }
 
-let string1 = String::from("long string is long");
-let result;
-{
-    let string2 = String::from("short");
-    result = longest(string1.as_str(), string2.as_str()); //! usually error occurs here
-}
-println!("The longest string is: {}", result);
+main();
